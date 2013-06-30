160974acb0156b7c9f31ba76191eb08f6924098c
Edgar Neto <edgar.figueiredo@gmail.com>
2013-06-30 03:37:42 +0200

    Finishing Task list impl.

--- src/chrome/js/journey.js
+++ src/chrome/js/journey.js
@@ -9,45 +9,71 @@ journey.filter('daysLeft', function() {
     }
 });
 
-journey.directive('otherName', function() {
-    return function(scope, element)
-    {
-        console.log('test');
-    };
-});
-
 function GoalsController($scope)
 {
     $scope.loaded = false;
     $scope.newGoal = null;
-    $scope.db = {'goals':[]};
+    $scope.db = {};
 
     $scope.load = function()
     {
-        chrome.storage.local.get('goals', function(data)
+        chrome.storage.sync.get('goals', function(data)
             {
                 $scope.loaded = true;
+                if (! (data.goals))
+                {
+                    data.goals = [];
+                }
                 $scope.db = data;
-                $scope.$apply();  
+                $scope.$apply();
             });
     }
 
     $scope.addGoal = function()
     {
-        $scope.db.goals.push(
+        $scope.db.goals.splice(0, 0, 
             {
                 description:$scope.newGoal, 
-                deadline:Date.today().add(365).days().getTime()
+                deadline:Date.today().add(365).days().getTime(),
+                done:false
             }
         );
         $scope.newGoal = null; 
         $scope.save();
     }
 
-    $scope.save = function() {
-        chrome.storage.local.set({'goals':$scope.db.goals});
+    $scope.remove = function(index)
+    {
+        if (confirm('Do you really want to remove this goal?'))
+        {
+            $scope.db.goals.splice(index, 1);
+            $scope.save();
+        }
     }
 
+    $scope.save = function() {
+        chrome.storage.sync.set({'goals':$scope.db.goals});
+    }
+
+    $scope.toggle = function(goal) {
+        goal.done = !goal.done;
+        $scope.sort();
+        $scope.save();
+    }
+
+    $scope.sort = function(goal)
+    {
+        $scope.db.goals = _.sortBy($scope.db.goals, 'done');
+    }
+
+    $scope.checked = function(goal)
+    {
+        return goal.done ? "checked" : "";
+    }
+
+    $scope.is = function(test, caseTrue) {
+        return test ? caseTrue : "";
+    }
 }
 
 
